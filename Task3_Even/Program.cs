e8b1812c52f1ac0198dcc95390091a13c94503ea
Svetlana <khabarova09@rambler.ru>
2023-03-09 18:53:03 +0300

    Task3_Even

---
+++ Task3_Even/Program.cs
@@ -0,0 +1,35 @@
+﻿int a = 4;
+if (a % 2 == 0)
+{
+  Console.Write(a);
+  Console.WriteLine(" - да");
+}
+else
+{
+  Console.Write(a);
+  Console.WriteLine(" - нет");
+}
+
+int b = -3;
+if (b % 2 == 0)
+{
+  Console.Write(b);
+  Console.WriteLine(" - да");
+}
+else
+{
+  Console.Write(b);
+  Console.WriteLine(" - нет");
+}
+
+int c=7;
+if(c%2==0)
+{
+Console.Write(c);
+Console.WriteLine(" - да");
+}
+else
+{
+  Console.Write(c);
+  Console.WriteLine(" - нет");
+}
