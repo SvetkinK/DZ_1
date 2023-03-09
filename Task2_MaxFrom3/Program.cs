b0d6e695bfddebb1a29d2cdd514f51e5accac8cf
Svetlana <khabarova09@rambler.ru>
2023-03-09 17:48:25 +0300

    Task2_MaxFrom3

---
+++ Task2_MaxFrom3/Program.cs
@@ -0,0 +1,37 @@
+﻿int a = 2;
+int b = 3;
+int c = 7;
+
+int max = a;
+if (a > max) max = a;
+if (b > max) max = b;
+if (c > max) max = c;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+
+int d = 44;
+int f = 5;
+int g = 78;
+
+int max1 = d;
+if (d > max) max = d;
+if (f > max) max = f;
+if (g > max) max = g;
+
+Console.Write("max = ");
+Console.WriteLine(max1);
+
+
+int a1 = 22;
+int b1 = 3;
+int c1 = 9;
+
+int max2 = a1;
+if (a1 > max) max = a1;
+if (b1 > max) max = b1;
+if (c1 > max) max = c1;
+
+Console.Write("max = ");
+Console.WriteLine(max2);
