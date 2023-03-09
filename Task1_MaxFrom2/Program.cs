c535f9595fc0ad63be33357911ca8261a4c399a4
Svetlana <khabarova09@rambler.ru>
2023-03-09 19:40:39 +0300

    Task1_MaxFrom2_1

--- Task1_MaxFrom2/Program.cs
+++ Task1_MaxFrom2/Program.cs
@@ -5,7 +5,11 @@ int max1 = a;
 if (a > max1) max1 = a;
 if (b > max1) max1 = b;
 
-Console.Write("max = ");
+Console.Write("из ");
+Console.Write(a);
+Console.Write(" и ");
+Console.Write(b);
+Console.Write(" max = ");
 Console.WriteLine(max1);
 
 
@@ -13,10 +17,14 @@ int c = 2;
 int d = 10;
 
 int max = c;
-if(c > max) max = c;
-if(d > max) max =d;
+if (c > max) max = c;
+if (d > max) max = d;
 
-Console.Write("max = ");
+Console.Write("из ");
+Console.Write(c);
+Console.Write(" и ");
+Console.Write(d);
+Console.Write(" max = ");
 Console.WriteLine(max);
 
 
@@ -24,8 +32,12 @@ int f = -9;
 int g = -3;
 
 int max2 = f;
-if(f > max2) max2 = f;
-if(g > max2) max2 = g;
+if (f > max2) max2 = f;
+if (g > max2) max2 = g;
 
-Console.Write("max = ");
+Console.Write("из ");
+Console.Write(f);
+Console.Write(" и ");
+Console.Write(g);
+Console.Write(" max = ");
 Console.WriteLine(max2);
