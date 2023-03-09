dced162779b1ae9871498cc57a49579130ba4882
Svetlana <khabarova09@rambler.ru>
2023-03-09 17:21:13 +0300

    Task1_MaxFrom2

---
+++ Task1_MaxFrom2/Program.cs
@@ -0,0 +1,31 @@
+﻿int a = 5;
+int b = 7;
+
+int max1 = a;
+if (a > max1) max1 = a;
+if (b > max1) max1 = b;
+
+Console.Write("max = ");
+Console.WriteLine(max1);
+
+
+int c = 2;
+int d = 10;
+
+int max = c;
+if(c > max) max = c;
+if(d > max) max =d;
+
+Console.Write("max = ");
+Console.WriteLine(max);
+
+
+int f = -9;
+int g = -3;
+
+int max2 = f;
+if(f > max2) max2 = f;
+if(g > max2) max2 = g;
+
+Console.Write("max = ");
+Console.WriteLine(max2);
