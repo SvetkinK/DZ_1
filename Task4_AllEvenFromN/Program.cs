fae19938770731c01924e4181e1ceb773d378387
Svetlana <khabarova09@rambler.ru>
2023-03-10 11:45:43 +0300

    Task4_AllEvenFromN

---
+++ Task4_AllEvenFromN/Program.cs
@@ -0,0 +1,21 @@
+﻿int n;
+//n = 5;
+n = 8;
+int[] array = new int[n];
+int index;
+
+for (index = 0; index <= n - 1; index++)
+{
+  array[index] = index + 1;
+}
+int even;
+
+for (index = 0; index <= n - 1; index++)
+{
+  if (array[index] % 2 == 0)
+  {
+    even = array[index];
+    Console.Write(even);
+    Console.Write(" ");
+  }
+}
